c807f138e278bc79a8dc189de614eec370c19229
ilham-s-saksena <ilhamsidikc@gmail.com>
2023-12-19 14:34:57 +0700

    second commit

---
+++ REST_API/flutter_image/upload_image.php
@@ -0,0 +1,76 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'db_con.php';
+
+// Check if the request is a POST request
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Check if the necessary data is present in the request
+    if (isset($_FILES["image"]) && isset($_POST["description"])) {
+        $image = $_FILES["image"];
+        $description = $_POST["description"];
+        $name = $_POST["name"];
+
+        // Validate file type using exif_imagetype
+        $imageType = exif_imagetype($image["tmp_name"]);
+        $allowedTypes = [IMAGETYPE_JPEG, IMAGETYPE_PNG];
+
+        if (!in_array($imageType, $allowedTypes)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Invalid file type"]);
+            exit;
+        }
+
+        // Set your upload directory
+        $uploadDir = "uploads/";
+        // Generate a unique name for the image
+        $imageName = uniqid() . '_' . $image["name"];
+        $uploadPath = $uploadDir . $imageName;
+
+        // Move the uploaded file to the specified directory
+        if (move_uploaded_file($image["tmp_name"], $uploadPath)) {
+            // Image uploaded successfully, now insert details into the database
+            $conn = connectToDatabase();
+
+            // Store the complete file path in the database
+            $filePath = $_SERVER['DOCUMENT_ROOT'] . '/' . "flutter_image/" . $uploadPath;
+
+            $sql = "INSERT INTO uploaded_images (file_name, name, description, file_path) VALUES ('$imageName', '$name', '$description', '$filePath')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo json_encode(["message" => "Image uploaded and details inserted into the database"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error inserting data into the database"]);
+                http_response_code(500);
+            }
+
+            $conn->close();
+        } else {
+            // Failed to move the file
+            http_response_code(500);
+            echo json_encode(["message" => "Failed to upload image"]);
+        }
+    } else {
+        // Data missing in the request
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid request data"]);
+    }
+} elseif ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $conn = connectToDatabase();
+    $sql = "SELECT * FROM uploaded_images";
+    $result = $conn->query($sql);
+
+    if($result) {
+        $user = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($user);
+    } else {
+        echo "Error: " . $conn->error; // Menampilkan pesan kesalahan dari MySQL jika ada kesalahan dalam query
+    }
+} else {
+    // Method not allowed
+    http_response_code(405);
+    echo json_encode(["message" => "Method Not Allowed"]);
+}
+?>
